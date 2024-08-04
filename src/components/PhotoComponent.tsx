68e67797fee3dc5ac4c9f273b9c893960736e990
marley.anthony <marleycanthony@gmail.com>
2024-08-03 22:31:58 -0700

    Add photo component

---
+++ src/components/PhotoComponent.tsx
@@ -0,0 +1,51 @@
+import { createApi } from "unsplash-js";
+import { Photo } from "../Types/Types";
+import { useEffect, useState } from "react";
+
+const PhotoComponent = () => {
+  const [photoResponse, setPhotosResponse] = useState<any | null>(null);
+  const unsplash = createApi({
+    accessKey: "bHUc8RD8oZ43PJAzlLSU79uLGtsUr-TyEWKiJRP4a-c",
+  });
+
+  useEffect(() => {
+    unsplash.search
+      .getPhotos({ query: "basketball", orientation: "landscape" })
+      .then((result) => {
+        setPhotosResponse(result);
+      })
+      .catch(() => {
+        console.log("something went wrong!");
+      });
+  });
+
+  const PhotoComp: React.FC<{ photo: Photo }> = ({ photo }) => {
+    const { user, urls } = photo;
+
+    return (
+      <>
+        <img className="img" src={urls.regular} alt="filler" />
+        <a
+          className="credit"
+          target="_blank"
+          href={`https://unsplash.com/@${user.username}`}
+          rel="noreferrer"
+        >
+          {user.name}
+        </a>
+      </>
+    );
+  };
+
+  if (photoResponse === null) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="feed">
+        <PhotoComp photo={photoResponse.response.results[0]} />
+      </div>
+    );
+  }
+};
+
+export default PhotoComponent;
